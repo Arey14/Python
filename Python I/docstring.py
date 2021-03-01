d99b784cf1accce224bd43b1e8f2d2c0a5bbfd19
Augusto Rey <augustorey_@hotmail.com>
2021-03-01 19:19:45 -0300

    Documentar

---
+++ Python I/docstring.py
@@ -0,0 +1,7 @@
+def suma(cualquier_parametro):
+    """Descripcion de lo que hace la funcion
+    cualquier_parametro int cualquier entero
+    returns cualquier_parametro + 5
+    """
+    return cualquier_parametro + 5
+help(suma)
